9b7f7abc6b49fbc9078d2a17a3243755e7ce4156
latimeri <var.trusova@gmail.com>
2025-07-23 23:14:31 +0300

    добавлена проверка баланса кошелька

---
+++ api/error_helpers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+)
+
+// запись ошибки в лог, метода и uri запроса
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+// отправляет ошибку в формате json клиенту
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	err := app.writeJSON(w, status, JSONEnveloper{"ошибка": message}, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// ответ если не найден запрашиваемый ресурс
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	app.errorResponse(w, r, http.StatusNotFound, "запрашиваемый кошелёк не найден")
+}
+
+// ответ о внутренней ошибке сервера
+func (app *application) internalErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusInternalServerError, "сервер столкнулся с непредвиденной проблемой")
+	app.logError(r, err)
+}
